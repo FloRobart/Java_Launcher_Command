c68649e1590086c05fbf6c97b8222e990597912b
Floris <florisrobart.pro@gmail.com>
2023-10-12 12:58:55 +0200

    apprentissage de lecture de fichier présent dans un jar

--- src/main/java/com/jlc/app/Main.java
+++ src/main/java/com/jlc/app/Main.java
@@ -1,12 +1,19 @@
 package com.jlc.app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class Main 
 {
-    public static void main( String[] args )
+    public void test(String[] args)
     {
         System.out.println("length : " + args.length);
         if (args.length < 1)
@@ -19,5 +26,18 @@ public class Main
             System.out.println(args[0]);
 
         Arguments arguments = new Arguments(args);
+
+        try (InputStream in = getClass().getResourceAsStream("/jlc.xml");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            System.out.println("result scanner : " + reader.readLine());
+        } catch (Exception e) {
+            System.out.println("Exception : " + e.getMessage());
+        }
+    }
+
+
+    public static void main( String[] args )
+    {
+        new Main().test(args);
     }
 }
